1bc4837c2f36f4c150e6400b7b7344c2312b271b
Santiago Garza <sgarzaceja@me.com>
2016-03-13 17:42:54 -0400

    login partial done

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  root 'users/:id' => 'users#show'
+  root 'users#show'
 
-  get 'home' => 'users#index'
+  get '/home' => 'users#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
