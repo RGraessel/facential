ae17a08f1326b57059d3fa0af8d1a537a687a970
Ryan Graessel <rkg8fb@gmail.com>
2016-03-14 21:30:45 -0400

    added coursesusers join table back to schema and updated routes in courses show

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314231222) do
+ActiveRecord::Schema.define(version: 20160315011557) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
   end
 
-  add_index "courses", ["user_id"], name: "index_courses_on_user_id"
+  create_table "courses_users", force: :cascade do |t|
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "courses_users", ["course_id"], name: "index_courses_users_on_course_id"
+  add_index "courses_users", ["user_id"], name: "index_courses_users_on_user_id"
 
   create_table "lesson_progresses", force: :cascade do |t|
     t.integer  "lesson_id"
@@ -77,9 +84,6 @@ ActiveRecord::Schema.define(version: 20160314231222) do
     t.string   "user_role"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "course_id"
   end
-
-  add_index "users", ["course_id"], name: "index_users_on_course_id"
 
 end
