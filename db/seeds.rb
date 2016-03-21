29ef56475ef949c9de44f2a86e2077dc4e75bb48
Santiago Garza <sgarzaceja@me.com>
2016-03-21 16:22:59 -0400

    medals in place

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,19 @@
      user.password_digest = Faker::Internet.password
    end
 
-  Course.create(course_name: "ruby")
+  #  User.create(user_id: 6, first_name: "Samuel", last_name: "Samuels", user_role: student)
+
+  Course.create(id: 1, course_name: "ruby")
   Course.create(course_name: "ruby on rails")
   Course.create(course_name: "javascript")
 
-  Lesson.create(lesson_name: "ruby classes", lesson_description: "this course teaches all you need to know about ruby classes", topic_id: [5, 6, 7])
+  Topic.create(course_id: 1, topic_name: "ruby classes 1", topic_id: 1)
+  Topic.create(course_id: 1, topic_name: "ruby classes 2", topic_id: 2)
+  Topic.create(course_id: 1, topic_name: "ruby classes 3", topic_id: 3)
+  Topic.create(course_id: 1, topic_name: "ruby variables 1", topic_id: 4)
 
-  Topic.create(topic_name: "ruby classes 1")
-  Topic.create(topic_name: "ruby classes 2")
-  Topic.create(topic_name: "ruby classes 3")
+  Lesson.create(id: 1, lesson_name: "ruby classes", lesson_description: "this course teaches all you need to know about ruby classes", topic_id: 1)
+  Lesson.create(id: 2, lesson_name: "ruby variables", lesson_description: "This course teaches all you need to know about ruby variables", topic_id: 1)
+
+  LessonResponses.create(marked_as_complete: true, lesson_id: 1, user_id: 6)
+  LessonResponses.create(marked_as_complete: true, lesson_id: 2, user_id: 6)
