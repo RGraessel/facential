45237da2fed9f54709dc74dfc45a53276ff5f25b
Santiago Garza <sgarzaceja@me.com>
2016-03-20 22:41:16 -0400

    homepage started

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
 $('#login').click(function(){
 $('#user_nav_partial').animate({width: 'toggle'}, 200);
+// $('#user_nav_partial').fadeToggle('fast', 'linear');
 // $('img.profile_pic').animate({width: 'toggle'}, 200);
 });
 });
