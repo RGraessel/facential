1d63f0be77c61c5c33a4f02067ef485031177952
Santiago Garza <sgarzaceja@me.com>
2016-03-30 18:51:38 -0400

    gemfile modified and ready for heroku deploy

--- Gemfile
+++ Gemfile
@@ -9,8 +9,6 @@ gem 'opentok', '~> 2.3'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
 
-gem 'pg'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,3 @@
-
 # require "opentok"
 # OPENTOK_KEY = ENV["tok_box_api_key"]
 # OPENTOK_SECRET = ENV["tok_box_secret"]
@@ -90,99 +89,99 @@ class LessonsController < ApplicationController
   #     render json: {id: 'UNKNOWN'}
   #   end
   # end
-  #
+
   # def stop_recording
-  #   opentok = OpenTok::OpenTok.new OPENTOK_KEY, OPENTOK_SECRET
-  #   archive_id = params[:archive_id]
-  #   opentok.archives.stop_by_id archive_id
-  #   render json: {id: archive_id}
-  # end
-  #
-  # def view_archived_video
-  #   sleep 2
-  #   opentok = OpenTok::OpenTok.new OPENTOK_KEY, OPENTOK_SECRET
-  #   archive_id = params[:archive_id]
-  #   current_lesson_response = LessonResponse.find_by(archive_id: archive_id)
-  #   archive = opentok.archives.find archive_id
-  #   current_lesson_response.recording_url = archive.url
-  #
-  #   render json: archive.url
-  # end
-  #
-  # def rerecord
-  #   archive = LessonResponse.find_by(archive_id: params[:archive_id])
-  #   archive.destroy
-  #   respond_to do |format|
-  #     format.json { render json: archive, status: :ok }
-  #   end
-  #   last_five = LessonResponse.last(5).reverse
-  # end
-  #
-  # def submit
-  #   video_submission = LessonResponse.find_by(archive_id: params[:archive_id])
-  #
-  #   latest_submission = video_submission.update(marked_as_complete: true)
-  #
-  #   redirect_to user_courses_path(current_user)
-  #
-  # end
-  # GET /lessons/new
-  def new
-    @lesson = Lesson.new
-  end
+ #   opentok = OpenTok::OpenTok.new OPENTOK_KEY, OPENTOK_SECRET
+ #   archive_id = params[:archive_id]
+ #   opentok.archives.stop_by_id archive_id
+ #   render json: {id: archive_id}
+ # end
+ #
+ # def view_archived_video
+ #   sleep 2
+ #   opentok = OpenTok::OpenTok.new OPENTOK_KEY, OPENTOK_SECRET
+ #   archive_id = params[:archive_id]
+ #   current_lesson_response = LessonResponse.find_by(archive_id: archive_id)
+ #   archive = opentok.archives.find archive_id
+ #   current_lesson_response.recording_url = archive.url
+ #
+ #   render json: archive.url
+ # end
+ #
+ # def rerecord
+ #   archive = LessonResponse.find_by(archive_id: params[:archive_id])
+ #   archive.destroy
+ #   respond_to do |format|
+ #     format.json { render json: archive, status: :ok }
+ #   end
+ #   last_five = LessonResponse.last(5).reverse
+ # end
+ #
+ # def submit
+ #   video_submission = LessonResponse.find_by(archive_id: params[:archive_id])
+ #
+ #   latest_submission = video_submission.update(marked_as_complete: true)
+ #
+ #   redirect_to user_courses_path(current_user)
+ #
+ # end
+ # GET /lessons/new
+ def new
+   @lesson = Lesson.new
+ end
 
-  # GET /lessons/1/edit
-  def edit
-  end
+ # GET /lessons/1/edit
+ def edit
+ end
 
-  # POST /lessons
-  # POST /lessons.json
-  def create
-    @lesson = Lesson.new(lesson_params)
+ # POST /lessons
+ # POST /lessons.json
+ def create
+   @lesson = Lesson.new(lesson_params)
 
-    respond_to do |format|
-      if @lesson.save
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
-        format.json { render :show, status: :created, location: @lesson }
-      else
-        format.html { render :new }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   respond_to do |format|
+     if @lesson.save
+       format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
+       format.json { render :show, status: :created, location: @lesson }
+     else
+       format.html { render :new }
+       format.json { render json: @lesson.errors, status: :unprocessable_entity }
+     end
+   end
+ end
 
-  # PATCH/PUT /lessons/1
-  # PATCH/PUT /lessons/1.json
-  def update
-    respond_to do |format|
-      if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lesson }
-      else
-        format.html { render :edit }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ # PATCH/PUT /lessons/1
+ # PATCH/PUT /lessons/1.json
+ def update
+   respond_to do |format|
+     if @lesson.update(lesson_params)
+       format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
+       format.json { render :show, status: :ok, location: @lesson }
+     else
+       format.html { render :edit }
+       format.json { render json: @lesson.errors, status: :unprocessable_entity }
+     end
+   end
+ end
 
-  # DELETE /lessons/1
-  # DELETE /lessons/1.json
-  def destroy
-    @lesson.destroy
-    respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+ # DELETE /lessons/1
+ # DELETE /lessons/1.json
+ def destroy
+   @lesson.destroy
+   respond_to do |format|
+     format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
+     format.json { head :no_content }
+   end
+ end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lesson
-      @lesson = Lesson.find(params[:id])
-    end
+ private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_lesson
+     @lesson = Lesson.find(params[:id])
+   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lesson_params
-      params.require(:lesson).permit(:lesson_name, :lesson_description, :lesson_objective, :topic_id)
-    end
+   # Never trust parameters from the scary internet, only allow the white list through.
+   def lesson_params
+     params.require(:lesson).permit(:lesson_name, :lesson_description, :lesson_objective, :topic_id)
+   end
 end
